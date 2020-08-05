c01ea401c08d07f98a1256bf5cfb804a01e8a683
narine9126 <narine9126@gmail.com>
2020-08-05 12:48:32 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test{
  public static void main(String[] args){
-  System.out.println("This is Test class"); 
+  System.out.println("This is Test class");
+  System.out.println("this is another ststement");
  }
 
 }
